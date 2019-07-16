b4a8d58f196e29fd3679cbcd1d7d15e5e096be56
Dagg <theamazingdoge91@gmail.com>
2019-07-16 04:28:52 -0700

    Simple population control
    you wont outlast humans at this rate

---
+++ Fox Clicker/Assets/Scripts/Population.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Population : MonoBehaviour
+{
+    public int foxPop = 0;
+    public Text text;
+    public Button spawnFoxButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnFoxButton.onClick.AddListener(spawnButtonPress);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnButtonPress()
+    {
+        foxPop++;
+        text.text = "" + foxPop;
+    }
+}
