9a8ad6ea1aa46bb52e3655c567055ccab744014d
Dagg <theamazingdoge91@gmail.com>
2019-07-16 05:03:55 -0700

    You can plant fox seeds now

--- Fox Clicker/Assets/Scripts/Population.cs
+++ Fox Clicker/Assets/Scripts/Population.cs
@@ -6,24 +6,47 @@ using UnityEngine.UI;
 public class Population : MonoBehaviour
 {
     public int foxPop = 0;
-    public Text text;
+    public Text popText;
+
     public Button spawnFoxButton;
+
+    public Button plantSeedButton;
+    int foxSeedPayout = 0;
+    int foxSeeds = 0;
+    public Text seedText;
 
     // Start is called before the first frame update
     void Start()
     {
         spawnFoxButton.onClick.AddListener(spawnButtonPress);
+        plantSeedButton.onClick.AddListener(plantButtonPress);
+        StartCoroutine(foxSeedInterval());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        popText.text = "" + foxPop;
     }
 
     void spawnButtonPress()
     {
         foxPop++;
-        text.text = "" + foxPop;
+    }
+
+    void plantButtonPress() {
+        if(foxPop >= 25) {
+            foxSeedPayout += 5;
+            foxPop -= 25;
+            foxSeeds++;
+            seedText.text = "" + foxSeeds;
+        }
+    }
+
+    IEnumerator foxSeedInterval(){
+        while(true) {
+            yield return new WaitForSeconds(5);
+            foxPop += foxSeedPayout;
+        }
     }
 }
