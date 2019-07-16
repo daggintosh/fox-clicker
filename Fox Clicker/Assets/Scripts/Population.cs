30a275683ecc799aef718872ed2e1edcf6dcf236
Dagg <theamazingdoge91@gmail.com>
2019-07-16 07:54:55 -0700

    Numerous fixes
    Made the population a 64 bit integer so the fox population isnt limited to 2 billion.
    
    Fixed issues with the time machine
    
    Added a cloning facility

--- Fox Clicker/Assets/Scripts/Population.cs
+++ Fox Clicker/Assets/Scripts/Population.cs
@@ -6,32 +6,39 @@ using UnityEngine.UI;
 public class Population : MonoBehaviour
 {
     #region Population Initiation
-    public int foxPop = 0;
+    public long foxPop = 0;
     public Text popText;
     public Button spawnFoxButton;
     #endregion
 
     #region Plant Fox Seed
     public Button plantSeedButton;
-    int foxSeedPayout = 0;
-    int foxSeeds = 0;
+    public Text seedText, seedPriceText;
     double seedPrice = 25;
-    public Text seedText;
-    public Text seedPriceText;
+    int foxSeedPayout, foxSeeds = 0;
     #endregion
 
     #region Give Fox Gun
     public Button sendFoxGun;
-    public Text gunText;
-    int foxGunPayout = 0;
-    int foxGuns = 0;
+    public Text gunText, gunPriceText;
+    double gunPrice = 200;
+    int foxGunPayout, foxGuns = 0;
+    #endregion
+
+    #region Start Cloning Foxes
+    public Button startFoxFactory;
+    public Text factoryText, factoryPriceText;
+    double factoryPrice = 500;
+    int foxFactoryPayout, foxFactories = 0;
     #endregion
 
     void Start()
     {
+        Time.timeScale = 1;
         spawnFoxButton.onClick.AddListener(spawnButtonPress);
         plantSeedButton.onClick.AddListener(plantButtonPress);
         sendFoxGun.onClick.AddListener(gunButtonPress);
+        startFoxFactory.onClick.AddListener(factoryButtonPress);
     }
 
     void Update()
@@ -71,13 +78,15 @@ public class Population : MonoBehaviour
 
     void gunButtonPress() 
     {
-        switch (foxPop >= 200)
+        switch (foxPop >= (int)gunPrice)
         {
             case true:
                 foxGunPayout += 20;
-                foxPop -= 200;
+                foxPop -= (int)gunPrice;
                 foxGuns++;
                 gunText.text = "" + foxGuns;
+                gunPrice *= 1.03;
+                gunPriceText.text = "" + (int)gunPrice;
                 break;
             default:
                 break;
@@ -92,10 +101,35 @@ public class Population : MonoBehaviour
         }
     }
 
+    void factoryButtonPress()
+    {
+        switch (foxPop >= (int)factoryPrice)
+        {
+            case true:
+                foxFactoryPayout += 43;
+                foxPop -= (int)factoryPrice;
+                foxFactories++;
+                factoryText.text = "" + foxFactories;
+                factoryPrice *= 1.09;
+                factoryPriceText.text = "" + (int)factoryPrice;
+                break;
+            default:
+                break;
+        }
+        switch(foxFactories == 1) 
+        {
+            case true:
+                StartCoroutine(foxFactoryInterval());
+                break;
+            default:
+                break;
+        }
+    }
+
     IEnumerator foxSeedInterval()
     {
         while(true) {
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSecondsRealtime(5);
             foxPop += foxSeedPayout;
         }
     }
@@ -103,8 +137,16 @@ public class Population : MonoBehaviour
     IEnumerator foxGunInterval()
     {
         while(true) {
-            yield return new WaitForSeconds(4);
+            yield return new WaitForSecondsRealtime(4);
             foxPop += foxGunPayout;
+        }
+    }
+    
+    IEnumerator foxFactoryInterval()
+    {
+        while(true) {
+            yield return new WaitForSecondsRealtime(2);
+            foxPop += foxFactoryPayout;
         }
     }
 }
